2e9dc1be296318738789df23f9d8275a915a08a4
sakthilakshmi11 <sakthilakshmi.lv2024aiml@sece.ac.in>
2025-12-18 15:43:07 +0530

    commitproduct

--- react_app/src/Products.jsx
+++ react_app/src/Products.jsx
@@ -17,7 +17,7 @@ export default function Products({ addToCart }) {
       const token = localStorage.getItem("token");
 
       try {
-        const res = await fetch("http://localhost:5000/api/products", {
+        const res = await fetch("http://51.20.35.49:5000/api/products", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
